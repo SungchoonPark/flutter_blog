00b5c0467e4d5e3b8cc40958d95b08be084c49ce
choon <choon@naver.com>
2022-03-10 19:31:07 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/pages/post/home_page.dart';
-import 'package:flutter_blog/pages/user/join_page.dart';
-import 'package:flutter_blog/pages/user/login_page.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -14,7 +12,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       // 라우트 설계 필요없음. GetX 라이브러리 사용 예정
-      home: JoinPage(
+      home: HomePage(
 
       ),
     );
